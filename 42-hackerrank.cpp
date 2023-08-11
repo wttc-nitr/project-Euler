060174a1045a16e32cfa265df2d25a434399f5e7
Animesh Kumar <kumaranimesh097@gmail.com>
2023-08-11 17:54:30 +0530

    two methods

--- 42-hackerrank.cpp
+++ 42-hackerrank.cpp
@@ -13,6 +13,26 @@ using namespace std;
 #define INF (1LL << 60)
 #define inf (1 << 30)
 
+/*
+ *      x = n * (n + 1) / 2
+ * =>   n = (-1 + sqrt(1 + 8x)) / 2   -> solution of quadratic equation
+ * =>   n must be an integer, that means sqrt(1 + 8x) is also integer
+ *
+ *  check whether (1 + 8x) is integer
+*/
+
+void altSolve (void) {
+    ll x; cin >> x;
+
+    ll y = sqrt(1 + 8 * x);
+
+    if (y * y == 1 + 8 * x)
+        cout << (y-1) / 2;
+    else cout << -1 ;
+
+    cout << '\n';
+}
+
 // ll x = 1e12;
 // x * (x + 1) -> doesn't overflow, 1e13 will overflow
 
@@ -42,7 +62,7 @@ int main(void) {
      
     int test_cases = 1; cin >> test_cases;
     while (test_cases-- > 0) {
-           solve();
+           altSolve();
     }
 
 #ifndef ONLINE_JUDGE
